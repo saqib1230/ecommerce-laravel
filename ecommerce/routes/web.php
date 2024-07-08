dc6073c9f0c88e8b5dae941e265697e3ab051d77
saqib iqbal <saqib1230@gmail.com>
2024-07-08 18:15:08 +0500

    make first route

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/admin')->namespace('app\http\controllers\admin')->group(function(){
+
+
+    Route::get('dashboard','AdminController@dashboard');
+
+});
